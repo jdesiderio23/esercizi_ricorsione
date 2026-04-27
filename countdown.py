e72c2b005f29a3b429260cb5f48e062c70bf8139
Jay <s333773@studenti.polito.it>
2026-04-27 21:29:14 +0200

    lezione 13/04 30:30

--- countdown.py
+++ countdown.py
@@ -7,15 +7,17 @@ def countdown(n):
         n -= 1
 
 def countdown_recursive(n):
-    #condizione terminale
+    # condizione terminale
     if n == 0:
         print("Stop")
-    #condizione non terminale
+    # condizione non terminale
     else:
         print(n)
-        # sleep(1)
+        sleep(1)
         countdown_recursive(n-1)
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     N = 4
     countdown_recursive(N)
