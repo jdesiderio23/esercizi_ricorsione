b21e394034c3579c2feec250b604dcdfaa556061
Jay <s333773@studenti.polito.it>
2026-04-28 18:16:57 +0200

    fine lezione 13/04 pt. 2

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,32 @@
+from functools import lru_cache
 from time import time
 
 
 class Fibonacci:
 
     def __init__(self):
-        pass
+        self.cache = {0: 0, 1: 1}
+
+    def calcola_elemento_cache(self, n):
+        # se ho già la soluzione per questo n la prendo dalla cache
+        if self.cache.get(n) is not None:
+            return self.cache[n]
+        # altrimenti devo andare avanti con la ricorsione
+        else:
+            self.cache[n] = (self.calcola_elemento_cache(n-1) +
+             self.calcola_elemento_cache(n-2))
+            return self.cache[n]
+
+    @lru_cache(maxsize=None)
+    def calcola_elemento_lru(self, n):
+        # caso terminale
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        # caso ricorsivo
+        else:
+            return self.calcola_elemento_lru(n-1) + self.calcola_elemento_lru(n-2)
 
     def calcola_elemento(self, n):
         # caso terminale
@@ -14,12 +36,23 @@ class Fibonacci:
             return 1
         # caso ricorsivo
         else:
-            return self.calcola_elemento(n-1) + self.calcola_elemento(n-2)
+            return self.calcola_elemento(n - 1) + self.calcola_elemento(n - 2)
+
 
 if __name__ == "__main__":
-    N = 40
+    N = 30
     fib = Fibonacci()
     start_time = time()
     print(fib.calcola_elemento(N))
     end_time = time()
     print(f"Elapsed time - recursion: {end_time - start_time}")
+
+    start_time = time()
+    print(fib.calcola_elemento_cache(N))
+    end_time = time()
+    print(f"Elapsed time - cache: {end_time - start_time}")
+
+    start_time = time()
+    print(fib.calcola_elemento_lru(N))
+    end_time = time()
+    print(f"Elapsed time - lru: {end_time - start_time}")
