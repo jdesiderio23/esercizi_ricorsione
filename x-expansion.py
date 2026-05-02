fcc7ae0c6ebff77878690417a21d99ee908ce25e
Jay <s333773@studenti.polito.it>
2026-05-02 18:26:05 +0200

    lezione 20/04 pt.1 29:00

--- x-expansion.py
+++ x-expansion.py
@@ -1,6 +1,7 @@
 class XExpansion:
     def __init__(self):
         self.soluzioni = []
+        self.soluzioni_list = []
 
     def calcola_list(self, input):
         self.soluzioni = []
@@ -8,18 +9,24 @@ class XExpansion:
 
     # parziale è la soluzione parziale
     # rimanenti sono il resto dei caratteri da esaminare
-    def _ricorsione(self, parziale: str, rimanenti: str):
+    def _ricorsione_list(self, parziale: list, rimanenti: str):
         # caso terminale
         if len(rimanenti) == 0:
             # print(parziale)
-            self.soluzioni.append(parziale)
+            self.soluzioni_list.append(parziale)
         # caso ricorsivo
         else:
             if rimanenti[0] == "X":
-                self._ricorsione(parziale + "0", rimanenti[1:])
-                self._ricorsione(parziale + "1", rimanenti[1:])
+                parziale.append("0")
+                self._ricorsione_list(parziale, rimanenti[1:])
+                parziale.pop()
+
+                parziale.append("1")
+                self._ricorsione_list(parziale, rimanenti[1:])
+                parziale.pop()
             else:
-                self._ricorsione(parziale + rimanenti[0], rimanenti[1:])
+                parziale.append(rimanenti[0])
+                self._ricorsione_list(parziale, rimanenti[1:])
 
     def calcola(self, input):
         self.soluzioni = []
@@ -72,5 +79,11 @@ def x_expansion2(input):
 if __name__ == "__main__":
     sequenza = "01X0X"
     xexp = XExpansion()
+
+    # metodo con soluzioni parziali rappresentate come stringhe
     xexp.calcola(sequenza)
     print(xexp.soluzioni)
+
+    # metodo con soluzioni parziali rappresentate come liste
+    xexp.calcola_list(sequenza)
+    print(xexp.soluzioni_list)
