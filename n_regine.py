43e0ddef1c1c4d0921f09d1d60fede67870c051b
Jay <s333773@studenti.polito.it>
2026-05-11 18:43:51 +0200

    lezione 20/04 pt.2 25:00

--- n_regine.py
+++ n_regine.py
@@ -1,13 +1,23 @@
 
 
 class NRegine():
-
-
     def __init__(self):
         pass
 
-    def solve(self, N):
-        pass
+    #===========================================APPROCCIO 2=====================================
+    # Rappresentiamo soluzione come un vettore di N regine,
+    # ognuno rappresentante una regina come riga e colonna
+    def solve2(self, N):
+        self._ricorsione2([], N)
+
+    # parziale è un vettore di coppie (riga, colonna)
+    def _ricorsione2(self, parziale, N):
+        # caso terminale: ho messo N regine
+        if len(parziale) == N:
+            print(parziale)
+        # caso ricorsivo: ho messo < N regine
+        else:
+
 
 if __name__ == "__main__":
     nreg = NRegine()
